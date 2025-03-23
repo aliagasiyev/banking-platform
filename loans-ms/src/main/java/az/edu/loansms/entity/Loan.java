f56975ab3e13b05a81f2ab0f45ced48dd7e74e0a
aliagasiyev <agasiyevali2@gmail.com>
2025-03-23 12:30:04 +0400

    Docker file and DB attached for loan-ms

---
+++ loans-ms/src/main/java/az/edu/loansms/entity/Loan.java
@@ -0,0 +1,19 @@
+package az.edu.loansms.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Entity
+@Data
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double amount;
+}

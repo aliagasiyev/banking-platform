477c16bcd0b33058ac22b31181f15a44e0a47389
aliagasiyev <agasiyevali2@gmail.com>
2025-03-23 22:33:11 +0400

    loan-ms attached

--- loans-ms/src/main/java/az/edu/loansms/entity/Loan.java
+++ loans-ms/src/main/java/az/edu/loansms/entity/Loan.java
@@ -1,19 +1,23 @@
 package az.edu.loansms.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "loan")
 @Data
+@NoArgsConstructor
 public class Loan {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Amount is required")
+    @Positive(message = "Amount must be greater than zero")
+    @Column(name = "amount", nullable = false)
     private Double amount;
 }
